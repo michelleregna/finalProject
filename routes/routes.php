1c539228a8dd1e6022a21e4b29a7d624ade1ce90
Michelle Regna <michelleregna@gmail.com>
2017-12-01 13:48:31 -0500

    added delete accounts

--- routes/routes.php
+++ routes/routes.php
@@ -103,6 +103,15 @@ class routes
         $route->method = 'delete';
         $routes[] = $route;
 
+        
+        $route = new route();
+        $route->http_method = 'POST';
+        $route->action = 'delete';
+        $route->page = 'accounts';
+        $route->controller = 'accountsController';
+        $route->method = 'delete';
+        $routes[] = $route;
+
         return $routes;
     }
 }
