f470898edcd8d8e16b17ddd6778d35c35ae10cac
Michelle Regna <michelleregna@gmail.com>
2017-12-01 13:54:03 -0500

    added delete button to show_account

--- pages/show_account.php
+++ pages/show_account.php
@@ -25,6 +25,10 @@ print_r($data);
 
 ?>
 
+<form action="index.php?page=accounts&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
+    <button type="submit" form="form1" value="delete">Delete</button>
+</form>
+
 
 <script src="js/scripts.js"></script>
 </body>
