24f0c4bc2bb815d2e2eee4b427dd79b476a5d862
Michelle Regna <michelleregna@gmail.com>
2017-12-01 14:01:16 -0500

    added header function to tasks delete

--- controllers/tasksController.php
+++ controllers/tasksController.php
@@ -63,7 +63,7 @@ class tasksController extends http\controller
     {
         $record = todos::findOne($_REQUEST['id']);
         $record->delete();
-        print_r($_POST);
+        header('Location: index.php?page=tasks&action=all');
 
     }
 
