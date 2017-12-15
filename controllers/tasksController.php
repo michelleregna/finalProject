69ad2eabcec5be8b8a6317b518fa932ce5295cfd
Michelle Regna <michelleregna@gmail.com>
2017-12-14 23:11:55 -0500

    updated all function

--- controllers/tasksController.php
+++ controllers/tasksController.php
@@ -10,8 +10,11 @@ class tasksController extends http\controller
 
     public static function all()
     {
-        $records = todos::findAll();
+        session_start();
+        $userID = $_SESSION['userID'];
+        $records = todos::findTasksById($userID);
         self::getTemplate('all_tasks', $records);
+       
 
     }
 
