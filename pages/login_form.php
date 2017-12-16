0920b4eb15db2e6f0b1de21e077ea71ad63f1e53
Michelle Regna <michelleregna@gmail.com>
2017-12-15 20:46:31 -0500

    updated login page

--- pages/login_form.php
+++ pages/login_form.php
@@ -1,19 +1,21 @@
+<h1> Login </h1>
 
-<div class="container">
-    <div class="row">
-    <div class="col-sm-6 col-md-4 col-md-offset-4">
-    <h1 class="text-center login-title">Log in</h1>
-    <div class="account-wall">
-    <form action="index.php?page=accounts&action=login" class="form-signin" method="POST">
-            <input type="text" class="form-control" name= "uname" placeholder="Email" required autofocus>
-            <input type="password" class="form-control" name="psw" placeholder="Password" required>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">
-                            Log in</button>
-            <label class="checkbox pull-left">
-            <input type="checkbox" value="remember-me">Remember me</label>
-    </form>
-    </div>
+ <div class="container">
+
+      <form action="index.php?page=accounts&action=login" class="form-signin" method="POST">
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name="uname" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="psw" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
+      </form>
     <a href="index.php?page=accounts&action=register" class="text-center new-account">Create an account</a>
     </div>
     </div>
 </div>
+
